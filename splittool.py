da8f5bf5f090b0c662c238e07ea5b74dd45e9cf2
iCrashed <bob@bob.(none)>
2013-04-16 23:08:05 +0545

    Slpittool version 2

--- splittool.py
+++ splittool.py
@@ -1,7 +1,14 @@
 """ This Program Splits Sentences """
 
-f = open("/home/bob/Desktop/Test.txt","r+")
-w = open("/home/bob/Desktop/TestNew.txt","w+")
+try:
+	f = open("/home/bob/Desktop/Test.txt","r+")
+except IOError:
+	print "Couldn't Open The File..."
+
+try:
+	w = open("/temp/splitedtext.txt","w+")
+except:
+	print "Couldn't Write a File in /temp/splitedtext.txt"
 
 text = f.read()
 
