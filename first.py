2cad13333b3c8440a29225299443835275530d7b
iCrashed <bob@bob.(none)>
2013-04-17 00:49:19 +0545

    Splittool 3rd Version

--- first.py
+++
@@ -1,2 +0,0 @@
-print "This is a sample file"
-print "testing"
--- splittool.py
+++ splittool.py
@@ -1,17 +1,37 @@
-""" This Program Splits Sentences """
+""" This Program Splits Sentences, works only with Linux OS """
+
+filepath = raw_input("Type the text file with the full path")
 
 try:
-	f = open("/home/bob/Desktop/Test.txt","r+")
-except IOError:
-	print "Couldn't Open The File..."
+	fileName, fileExt = filepath.split('.')	
 
-try:
-	w = open("/temp/splitedtext.txt","w+")
-except:
-	print "Couldn't Write a File in /temp/splitedtext.txt"
+except IndexError:
+	print "No File Included"
 
-text = f.read()
 
-NewText =  text.replace(".",".\n")
 
-w.write(NewText)
+
+if fileExt != '.txt':
+	print "The File is Not .txt"
+
+
+else:
+
+	try:
+		f = open(filepath,"r+")
+	except IOError:
+		print "Couldn't Open The File..."
+
+
+	try:
+		w = open("/tmp/splitedtext.txt","w+")
+	except IOError:
+		print "Couldn't Write a File /temp/splitedtext.txt"
+
+	text = f.read()
+
+	NewText =  text.replace(".",".\n")
+
+	w.write(NewText)
+
+	print "Splited text file saved in /tmp/splitedtext.txt"
