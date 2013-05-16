5f18fbaea66f1d89c4914123d0acbe3dfe6f29aa
iCrashed <bob@bob.(none)>
2013-05-17 00:34:13 +0545

    GUI with two text viewr, for Nepali and English

--- GUI.py
+++ GUI.py
@@ -7,11 +7,12 @@ from tkFileDialog import askopenfilename
 
 class mainFrame:
 
-
 	def __init__(self,root):
 		frame=Frame(root)
 		self.makeMenuBar(frame,root)
 #		self.txtfr(frame)
+		self.english_viewer(frame,root,'filename.txt')
+		self.nepali_viewer(frame,root,'filename.txt')
 		frame.pack()
 		return
 	#defines menubar
@@ -22,7 +23,8 @@ class mainFrame:
 		mb_file = Menubutton(menubar,text = 'File')
 		mb_file.pack(side = LEFT)
 		mb_file.menu = Menu(mb_file)
-		mb_file.menu.add_command(label = 'Open',command = self.file_open(frame))
+		mb_file.menu.add_command(label = 'Open English File',command = self.file_open_english(frame,root))
+		mb_file.menu.add_command(label = 'Open Nepali  File',command = self.file_open_nepali(frame,root))
                 mb_file.menu.add_command(label = 'Exit',command = root.destroy)
 		mb_edit = Menubutton(menubar,text = 'Edit')
 		mb_edit.pack(side = LEFT)
@@ -34,62 +36,68 @@ class mainFrame:
  		mb_edit['menu'] = mb_edit.menu
 		return
 	
-
         def convert(self):
             print "File Successfully converted"
 
-	#displays the files giving the user choice to choose  file
-	def file_open(self,frame):
+
+
+	def file_open_english(self,frame,root):
 		
 		root = Tk()
 		filename =askopenfilename(filetypes=[("textfiles","*.txt")])
+		self.english_viewer(frame,root,filename)
+	def english_viewer(self,frame,root,filename):
+		root = Tk()
+		fixedFont = Pmw.logicalfont('Fixed')
+		self.st = Pmw.ScrolledText(frame,
+					   labelpos = 'n',
+					   label_text=filename,
+					   usehullsize = 1,
+					   hull_width = 700,
+					   hull_height = 500,
+					   text_wrap='none',
+					   text_font = fixedFont,
+					   text_padx = 4,
+					   text_pady = 4,)
+		self.st.importfile(filename);
+		self.st.pack(padx = 5, pady = 5, fill = 'both', expand = 1)
+	
+		Pmw.initialise(root)
+
+
+	def file_open_nepali(self,frame,root):
+		
+		root = Tk()
+		filename =askopenfilename(filetypes=[("textfiles","*.txt")])
+		self.nepali_viewer(frame,root,filename)
+
+	def nepali_viewer(self,frame,root,filename):
 
 		fixedFont = Pmw.logicalfont('Fixed')
 		self.st = Pmw.ScrolledText(frame,
-			  labelpos = 'n',
-			  label_text=filename,
-
-
-		          usehullsize = 1,
-		          hull_width = 700,
-		          hull_height = 500,
-		          text_wrap='none',
-		          text_font = fixedFont,
-
-		           text_padx = 4,
-		           text_pady = 4,)
-	   
+					   labelpos = 'n',
+					   label_text=filename,
+					   
+					   usehullsize = 1,
+					   hull_width = 700,
+					   hull_height = 500,
+					   text_wrap='none',
+					   text_font = fixedFont,
+					   text_padx = 4,
+					   text_pady = 4,)
 		self.st.importfile(filename);
 		self.st.pack(padx = 5, pady = 5, fill = 'both', expand = 1)
-
+		
 		Pmw.initialise(root)
-   
 
-        # Prevent users' modifying text and headers
-#	self.st.configure(text_state = 'disabled')
-
- 
-
-
-
-
-
-"""
- self.dialog = 
-
-"""
-
-                
-
-       
-
+''' Class ends here '''
 
 def main():
 	root = Tk()
 	k = mainFrame(root)
 	root.title('Sentence Spliting Tool')
 
-	             #create button to convert file:
+	#create button to convert file:
         convertButton = Tkinter.Button(root,text='Convert') # change to Button(text='Convert',command = self.convert)
         convertButton.pack(side = BOTTOM)
 	root.mainloop()
@@ -97,29 +105,3 @@ def main():
  
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-""" 
-    exitButton = Tkinter.Button(root, text = 'Exit', command = root.destroy)
-
----------------------------------------------------------------------------------
-#This Is Not Needed For Now,
-
-	#defines the text area
-	def txtfr(self,frame):
-		textfr = Frame(frame)
-		self.text = Text(textfr,height = 10,width = 50,background='white')
-		scroll = Scrollbar(textfr)#Adding Scroll Bar
-		self.text.configure(yscrollcommand = scroll.set)
-		self.text.pack(side = LEFT)
-		scroll.pack(side = RIGHT,fill = Y)
-		textfr.pack(side = TOP)
-		return
-
-"""
-
-
