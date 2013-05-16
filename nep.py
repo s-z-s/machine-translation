2c9832a176337676e8a0af47a84dc110b41e437b
kunwararzun <kunwar.arzun@gmail.com>
2013-05-17 01:08:10 +0545

    Update nep.py

--- nep.py
+++ nep.py
@@ -1,6 +1,10 @@
 import codecs
 import re
-infile = codecs.open("nepali.txt", "r", "utf8")
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+infile = codecs.open(filename, "r", "utf8")
 outfile = codecs.open("output.txt", "w", "utf8")
 file = infile.read()
 end = re.compile(ur'([\u0964]\s|[\u003F]\s|[\u0021]\s)', re.UNICODE)
